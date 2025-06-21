f17b2b65121211712129b973c312c4e91652b3ea
Arthur Laterza <arthurvbl05@gmail.com>
2025-06-21 20:24:50 -0300

    atualização final da rcx
    finalização do código do seguidor que (quase) seguiu linha, contendo constantes, mecanismo de parada e etc.
    no mais foi um prazer imenso trabalhar com os senhores

--- codDefinitivo/codDefinitivo.ino
+++ codDefinitivo/codDefinitivo.ino
@@ -7,32 +7,113 @@ OrangutanMotors motors;
 
 #define SEN_D A6                        // ADC6
 #define BRUSHED 8                       // PB0
-#define MAX_DIREITA 4                   // Quantidade máxima de vezes que o sensor da direita lê linhas no circuito
+#define LOWH 10                         // PB2 (força sinal PWM low na ponte h)
+#define MAX_DIREITA 26                  // Quantidade máxima de vezes que o sensor da direita lê linhas no circuito
 #define LED_SENSOR 2
 
 QTRSensors qtr;
 
-//const uint8_t sensorCount = 6;        // ALTERAR CASO ESTEJA USANDO SOMENTE 6 SENSORES
-const uint8_t sensorCount = 8;
+const uint8_t sensorCount = 6;        // ALTERAR CASO ESTEJA USANDO SOMENTE 6 SENSORES
+//const uint8_t sensorCount = 8;
 uint16_t sensorValues[sensorCount];
 
 // ------------ Sensores Laterais --------------
+// sensores vão de 0(branco) a 1000(preto)
 int contadorDireita = 0;                // Conta quantas vezes leu a linha da direita
-const int threshold = 200;              // Valor intermediario em que o sensor considera como preto ou branco
+const int threshold = 910;              // Valor intermediario em que o sensor considera como preto ou branco
 
 bool seguindoLinha = false;
 bool modoLento = false;
 bool usandoLinhaPreta = false;
 bool estadoAnteriorDireita = false;
 
-int velocidadeLenta = 40;               // Velocidade base para o modo lento
-int centro = 3500;                      // Posição central corrigida dos sensores (0-5000)
+int velocidadeLenta = 60;               // Velocidade base para o modo lento
+int centro = 2500;                      // Posição central corrigida dos sensores (0-5000)
 
 
 // ------------ CONSTANTES PID ------------
+/*
+  TODOS OS TESTES FORAM FEITOS COM V75
+  TESTE 1 = SEGUIU LINHA
+  Kp = 0.04
+  Ki = 0.0005
+  Kd = 0.5
+  TESTE 2 = ROBÔ SEGUIU LINHA, MAS NÃO FOI BEM
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 0.0
+  TESTE 3 = FRACASSO
+  Kp = 0.1
+  Ki = 0.0
+  Kd = 0.0
+  TESTE 4 = ROBÔ SEGUIU LINHA, MAS NÃO FOI BEM
+  Kp = 0.06
+  Ki = 0.0
+  Kd = 0.0
+  TESTE 5 = ROBÔ SEGUIU LINHA, FOI MELHOR QUE O TESTE 1
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 0.1
+  TESTE 6 = ROBÔ SEGUIU LINHA, FOI MELHOR QUE O TESTE 5
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 0.2
+  TESTE 7 = ROBÔ SEGUIU LINHA, FOI MELHOR QUE O TESTE 6
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 0.3
+  TESTE 8 = ROBÔ SEGUIU LINHA, FOI MELHOR QUE O TESTE 7
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 0.4
+  TESTE 9 = ROBÔ SEGUIU LINHA, FOI MUITO MELHOR QUE O TESTE 8
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 0.5
+  TESTE 10 = ROBÔ SEGUIU LINHA, FOI MELHOR QUE O TESTE 9
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 0.7
+  TESTE 11 = ROBÔ SEGUIU LINHA, AINDA NÃO SEI JULGAR SE FOI MELHOR QUE O 10
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 0.8
+  TESTE 12 = ROBÔ SEGUIU LINHA, TIVE A IMPRESSÃO QUE O TESTE 11 FOI MELHOR
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 1.2
+  TESTE 13 = ROBÔ SEGUIU LINHA MAL, CORREÇÃO MUITO GROSSEIRA
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 1.7
+  TESTE 15 = ROBÔ SEGUIU LINHA MAL, APENAS ME CONFIRMOU QUE O KD ESTÁ ALTO
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 1.5
+  TESTE 15 = ROBÔ SEGUIU LINHA, DERRAPANDO UM POUCO
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 1.36
+  TESTE 16 = ROBÔ SEGUIU LINHA, ME FEZ PERCEBER QUE O TESTE 12 FOI MELHOR
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 1.3
+  TESTE 17 = ROBÔ SEGUIU LINHA, MUITO MELHOR QUE O TESTE 16
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 1.0
+  TESTE 18 = ROBÔ SEGUIU LINHA, PIOR QUE O TESTE 17
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 0.9
+  TESTE 19 = ROBÔ SEGUIU LINHA, DE LONGE O MELHOR TESTE
+  Kp = 0.04
+  Ki = 0.0
+  Kd = 1.02
+*/
 double Kp = 0.04;
-double Ki = 0.0005;
-double Kd = 0.5;
+double Ki = 0.0;
+double Kd = 1.02;
 
 int erroAnterior = 0;
 long erroIntegral = 0; 
@@ -41,12 +122,14 @@ void setup() {
   pinMode(LED_SENSOR, OUTPUT);
   digitalWrite(LED_SENSOR, HIGH); 
 
+  pinMode(LOWH, OUTPUT);
+
   pinMode(SEN_D, INPUT);
   pinMode(BRUSHED, OUTPUT);
 
   qtr.setTypeRC(); 
-  //qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, 7, 4}, sensorCount);          // Sensores de 1 a 6 (caso 8 sensores estiverem falhando)
-  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5, 7, 4}, sensorCount);    // Sensores de 1 a 8 (prototipo 3)
+  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5}, sensorCount);          // Sensores de 1 a 6 (caso 8 sensores estiverem falhando)
+  //qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5, 7, 4}, sensorCount);    // Sensores de 1 a 8 (prototipo 3)
 
   qtr.setEmitterPin(LED_SENSOR);
 
@@ -65,6 +148,7 @@ void loop() {
       4 = CALIBRAÇÃO DOS SENSORES FRONTAIS
       5 = MODO: SEGUIDOR DE LINHA LENTO
       6 = MOSTRA PARÂMETROS ATUAIS (VELOCIDADE, CONSTANTES PID E QUAL COR DE LINHA SEGUE)
+      7 = ATIVAÇÃO DOS MOTORES BRUSHED
       p = AJUSTA CONSTANTE P
       i = AJUSTA CONSTANTE I
       d = AJUSTA CONSTANTE D
@@ -130,6 +214,9 @@ void loop() {
       Serial.print("Modo: Seguindo linha ");
       Serial.println(usandoLinhaPreta ? "PRETA" : "BRANCA");
     }
+    else if (comando == "7"){
+      digitalWrite(BRUSHED, HIGH);
+    }
     else if (comando.startsWith("p")) { // Ajusta Kp
       Kp = comando.substring(1).toFloat();
       Serial.print("Novo Kp: "); Serial.println(Kp);
@@ -160,13 +247,14 @@ void loop() {
     }
     else if (comando == "r"){
       int direita = analogRead(SEN_D);
-      Serial.print(direita);
+      Serial.println(direita);
     }
   }
 
   // --- MODO PID: Seguir Linha ---
   if (seguindoLinha) {
     digitalWrite(BRUSHED, HIGH);
+    digitalWrite(LOWH, LOW);
     uint16_t pos = usandoLinhaPreta ? qtr.readLineBlack(sensorValues)
                                      : qtr.readLineWhite(sensorValues);
 
@@ -193,8 +281,8 @@ void loop() {
       // Garante que a velocidade não caia abaixo de um mínimo (ex: 60)
       velocidadeBase = constrain(velocidadeBase - (absErro / 10), velocidadeLenta, velocidadeBase);
     }
-    int velEsq = velocidadeBase + correcao;
-    int velDir = velocidadeBase - correcao;
+    int velEsq = velocidadeBase - correcao;
+    int velDir = velocidadeBase + correcao;
     velEsq = constrain(velEsq, 0, 255);
     velDir = constrain(velDir, 0, 255);
     
@@ -210,13 +298,14 @@ void loop() {
         Serial.println(contadorDireita);
 
         if (contadorDireita >= MAX_DIREITA) {
-            // anda um cadinho antes de parar
-            motors.setSpeeds(20, 20);
-            delay(500);
+          // anda um cadinho antes de parar
+          motors.setSpeeds(40, 40);
+          delay(100);
 
-            motors.setSpeeds(0, 0);
-            seguindoLinha = false;
-            Serial.println("Limite de flags da direita atingido. Robô PAROU.");
+          motors.setSpeeds(0, 0);
+          seguindoLinha = false;
+          delay(100000);
+          Serial.println("Limite de flags da direita atingido. Robô PAROU.");
         }
     }
     // Atualiza o estado anterior
